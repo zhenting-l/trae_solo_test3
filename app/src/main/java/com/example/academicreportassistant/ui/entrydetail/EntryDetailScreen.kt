ba211165def435dfc30f6d1e0715d1e3b21369d3
zhenting-l <1372407175@qq.com>
2026-05-08 18:52:45 +0800

    Update EntryDetailScreen.kt

--- app/src/main/java/com/example/academicreportassistant/ui/entrydetail/EntryDetailScreen.kt
+++ app/src/main/java/com/example/academicreportassistant/ui/entrydetail/EntryDetailScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
